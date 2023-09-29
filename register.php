bdb3af5af953760ea7e44ac3d98a44cab6439a52
Brian <kayangabrian24@gmail.com>
2023-09-29 21:57:03 +0300

    created form for registering

---
+++ register.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'User.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $user = new User();
+    if ($user->register($username, $password)) {
+        echo "Registration successful! <a href='login.php'>Login here</a>";
+    } else {
+        echo "Registration failed.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sign Up</title>
+</head>
+<body>
+    <h1>Sign Up</h1>
+    <form method="POST" action="">
+        <label for="username">Username:</label>
+        <input type="text" name="username" required><br>
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" required><br>
+
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
