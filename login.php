bf79b0458a9038fe14c48ec6f9c4fc33166300f2
Brian <kayangabrian24@gmail.com>
2023-09-29 22:14:36 +0300

    created login file

---
+++ login.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'User.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $user = new User();
+    $loggedInUser = $user->login($username, $password);
+
+    if ($loggedInUser) {
+        session_start();
+        $_SESSION['user_id'] = $loggedInUser['id'];
+        $_SESSION['username'] = $loggedInUser['username'];
+        header("Location: view_users.php");
+    } else {
+        echo "Login failed. Invalid username or password.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="login.css">
+
+</head>
+<body>
+    <h1>Login</h1>
+    <form method="POST" action="">
+        <label for="username">Username:</label>
+        <input type="text" name="username" required><br>
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" required><br>
+
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
